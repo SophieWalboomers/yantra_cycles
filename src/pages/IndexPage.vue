18acb51eecd41063b4a9f0b9eed9300d2c5f1c6a
Sophie Walboomers <sophie@MacBook-Pro-van-Sophie-3.local>
2026-01-15 16:40:03 +0100

    add birthday selector

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,7 +1,18 @@
 <template>
-  <q-page class="flex flex-center"> </q-page>
+  <q-page class="column items-center">
+    <q-separator class="q-my-lg" />
+    <div class="text-body1">Calculate your Yantra cycles!</div>
+
+    <q-separator class="q-my-lg" />
+
+    <q-card flat class="q-pa-md">
+      <div class="text-subtitle2 q-mb-sm">Select your birthday</div>
+      <q-date v-model="birthday" mask="YYYY-MM-DD" minimal />
+    </q-card>
+  </q-page>
 </template>
 
 <script setup>
-//
+import { ref } from 'vue'
+const birthday = ref('')
 </script>
