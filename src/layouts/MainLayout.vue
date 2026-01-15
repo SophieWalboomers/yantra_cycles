d7f404b356f9920944ec0ba7daa1eddc7d01c073
Sophie Walboomers <sophie@MacBook-Pro-van-Sophie-3.local>
2026-01-15 16:33:26 +0100

    basic layout

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,18 +1,24 @@
 <template>
   <q-layout view="lHh Lpr lFf">
-    <q-header elevated>
-      <q-toolbar>
+    <q-header elevated class="text-black" style="background-color: white">
+      <q-toolbar style="height: 6rem">
         <q-btn flat dense round icon="menu" aria-label="Menu" @click="toggleLeftDrawer" />
 
-        <q-toolbar-title> Quasar App </q-toolbar-title>
-
-        <div>Quasar v{{ $q.version }}</div>
+        <q-toolbar-title> Yantra cycle calculator </q-toolbar-title>
+        <div class="absolute-center">
+          <img
+            alt="Shri Yantra"
+            src="~assets/sri_yantra_image.webp"
+            style="width: 5rem; height: auto"
+          />
+        </div>
+        <div>By Sophie Walboomers</div>
       </q-toolbar>
     </q-header>
 
     <q-drawer v-model="leftDrawerOpen" show-if-above bordered>
       <q-list>
-        <q-item-label header> Essential Links </q-item-label>
+        <q-item-label header> Information per cycle number </q-item-label>
 
         <EssentialLink v-for="link in linksList" :key="link.title" v-bind="link" />
       </q-list>
@@ -30,46 +36,54 @@ import EssentialLink from 'components/EssentialLink.vue'
 
 const linksList = [
   {
-    title: 'Docs',
-    caption: 'quasar.dev',
-    icon: 'school',
-    link: 'https://quasar.dev',
+    title: '1-cycle',
+    // caption: 'quasar.dev',
+    // icon: '',
+    link: '',
   },
   {
-    title: 'Github',
-    caption: 'github.com/quasarframework',
-    icon: 'code',
-    link: 'https://github.com/quasarframework',
+    title: '2-cycle',
+    // caption: 'github.com/quasarframework',
+    // icon: 'code',
+    link: '',
   },
   {
-    title: 'Discord Chat Channel',
-    caption: 'chat.quasar.dev',
-    icon: 'chat',
-    link: 'https://chat.quasar.dev',
+    title: '3-cycle',
+    // caption: 'chat.quasar.dev',
+    // icon: 'chat',
+    link: '',
   },
   {
-    title: 'Forum',
-    caption: 'forum.quasar.dev',
-    icon: 'record_voice_over',
-    link: 'https://forum.quasar.dev',
+    title: '4-cycle',
+    // caption: 'forum.quasar.dev',
+    // icon: 'record_voice_over',
+    link: '',
   },
   {
-    title: 'Twitter',
-    caption: '@quasarframework',
-    icon: 'rss_feed',
-    link: 'https://twitter.quasar.dev',
+    title: '5-cycle',
+    // caption: '@quasarframework',
+    // icon: 'rss_feed',
+    link: '',
   },
   {
-    title: 'Facebook',
-    caption: '@QuasarFramework',
-    icon: 'public',
-    link: 'https://facebook.quasar.dev',
+    title: '6-cycle',
+    // caption: '@QuasarFramework',
+    // icon: 'public',
+    link: '',
   },
   {
-    title: 'Quasar Awesome',
-    caption: 'Community Quasar projects',
-    icon: 'favorite',
-    link: 'https://awesome.quasar.dev',
+    title: '7-cycle',
+    // caption: 'Community Quasar projects',
+    // icon: 'favorite',
+    link: '',
+  },
+  {
+    title: '8-cycle',
+    link: '',
+  },
+  {
+    title: '9-cycle',
+    link: '',
   },
 ]
 
