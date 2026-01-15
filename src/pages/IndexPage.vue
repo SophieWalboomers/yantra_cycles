9958e11acfa9110852b7366041639f0e6db7b7a1
Sophie Walboomers <sophie@MacBook-Pro-van-Sophie-3.local>
2026-01-15 16:46:26 +0100

    add calculations based on input birthday

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -9,10 +9,61 @@
       <div class="text-subtitle2 q-mb-sm">Select your birthday</div>
       <q-date v-model="birthday" mask="YYYY-MM-DD" minimal />
     </q-card>
+
+    <q-btn label="Calculate cycles" color="primary" @click="calculate" />
+
+    <template v-if="showResult && birthday">
+      <div class="text-body1 q-my-lg">
+        You are currently in a
+        <div>
+          <b>{{ yearCycle }}</b
+          >-cycle year
+        </div>
+        <div>
+          <b>{{ monthCycle }}</b
+          >-cycle month
+        </div>
+        <div>
+          <b>{{ dayCycle }}</b
+          >-cycle day
+        </div>
+      </div>
+    </template>
+
+    <template v-else-if="showResult && !birthday">
+      <div class="text-body1 q-my-lg">Please select your birthday first :)</div>
+    </template>
   </q-page>
 </template>
 
 <script setup>
-import { ref } from 'vue'
+import { computed, ref, watch } from 'vue'
 const birthday = ref('')
+
+const month = computed(() => (birthday.value ? Number(birthday.value.slice(5, 7)) : null))
+const day = computed(() => (birthday.value ? Number(birthday.value.slice(8, 10)) : null))
+
+const yearCycle = ref(null)
+const monthCycle = ref(null)
+const dayCycle = ref(null)
+
+const showResult = ref(false)
+
+watch(birthday, () => {
+  showResult.value = false
+})
+
+function mod9to1(n) {
+  return ((((n - 1) % 9) + 9) % 9) + 1
+}
+
+function calculate() {
+  const now = new Date()
+
+  yearCycle.value = mod9to1(month.value + day.value + now.getFullYear() + 1)
+  monthCycle.value = mod9to1(yearCycle.value - (7 - (now.getMonth() + 1)))
+  dayCycle.value = mod9to1(monthCycle.value + now.getDate())
+
+  showResult.value = true
+}
 </script>
